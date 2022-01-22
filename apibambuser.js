337fb2a1cde14447c81c4d56ddb1ecaab9be3736
app-pe <54180318+app-pe@users.noreply.github.com>
2022-01-22 10:40:25 -0500

    Update apibambuser.js

--- apibambuser.js
+++ apibambuser.js
@@ -204,7 +204,7 @@ function obtenerOpciones(product){
 
 function obtenerVariaciones(product){
     /* obtiene todos los atributos de los hijos */
-  var variaciones = [], dataName = [], data = [], data2 = [];
+  var variaciones = [], dataName = [], data = [];
   try{
     $.each(product.SKUs, function(k,i){
       $.each(i.Attributes, function(kk,ii){
@@ -215,16 +215,8 @@ function obtenerVariaciones(product){
                     });
         }
       });
-      $.each(i.prices, function(kk,ii){
-        if(ii.listPrice !== "" && ii.cardPrice !== ""){
-          data2.push({
-                    "listPrice": ii.listPrice,
-                    "cardPrice": ii.cardPrice
-                    });
-        }
-      });
     });
-    
+    console.log(data);
     /* por diferentes atributos */
     $.each(data, function(k,i){
       if(dataName.indexOf(i.name) === -1){
@@ -235,10 +227,8 @@ function obtenerVariaciones(product){
     /* estructura de api */
     $.each(dataName, function(k,i){
       var obj = {};
-      obj.title="Variacion"+k,
       obj.name = i;
       obj.optionId = k;
-      obj.prices = data2;
       obj.values = [];
       $.each(data, function(kk,ii){
         if(i === ii.name && (obj.values).indexOf(ii.value) === -1){
